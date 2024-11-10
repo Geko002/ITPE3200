88935f270d90485926fb6508af259bda43bcff6d
Qasim2512 <122743097+Qasim2512@users.noreply.github.com>
2024-11-10 16:27:29 +0100

    qasim

--- obj/Debug/net8.0/PixNote.AssemblyInfo.cs
+++ obj/Debug/net8.0/PixNote.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bba5958438765e331bf7cea3d7da8b915108b074")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aecd4437dcf33897a568c688a38dacdae4e04a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
